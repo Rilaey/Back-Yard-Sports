9e7e9874ccb252a79d248e85cbb9ed1d3e124453
Riley <94302477+Rilaey@users.noreply.github.com>
2023-01-19 18:45:45 -0500

    created api routes

---
+++ Develop/controllers/api/teamRoutes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const { Team } = require('../../models');
+const userAuth = require('../../utils/auth');
+
+// create new team
+router.post('/', userAuth, async (req, res) => {
+  try {
+    const newTeam = Team.create({
+      name: req.body.name,
+      captain: req.session.user_id,
+      players: req.session.user_id,
+      sport: req.body.sport,
+      state: req.body.state,
+      city: req.body.city,
+    });
+
+    res.status(200).json(newTeam);
+  } catch (err) {
+    res.status(500).json(err);
+    console.log(err);
+  }
+});
+
+module.exports = router;
