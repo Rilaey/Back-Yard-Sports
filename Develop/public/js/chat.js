138cbf98cae580b320ea9861ec7b6910aedf2796
Anthony <quaz711@hotmail.com>
2023-01-25 13:09:14 -0500

    chat working at this point. just needs styling

--- Develop/public/js/chat.js
+++ Develop/public/js/chat.js
@@ -1,22 +1,52 @@
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var socket = require('socket.io')(server);
-var messages = document.getElementById('messages');
-var form = document.getElementById('form');
+const socket = io();
+const form = document.getElementById('form');
 var input = document.getElementById('input');
+var messages = document.getElementById('messages');
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
+
+const { username, room } = Qs.parse(location.search, {
+    ignoreQueryPrefix: true
+});
+
+socket.emit('joinRoom', { username, room});
+
+socket.on('roomUsers', ({ room, users }) => {
+    outputRoomName(room);
+    outputUsers(users);
+});
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
     if (input.value) {
+        console.log(input.value);
         socket.emit('chat message', input.value);
         input.value = '';
+        input.focus();
     }
 });
 
-socket.on('chat message', function(msg) {
+/*socket.on('chat message', function(msg) {
     var item = document.createElement('li');
     item.textContent = msg;
     messages.appendChild(item);
     window.scrollTo(0, document.body.scrollHeight);
+    console.log(msg);
+});*/
+
+socket.on('chat message', function(msg) {
+    var item = document.createElement('div');
+    item.classList.add('msg');
+    item.innerHTML = `<p class="meta">${msg.username}<span>${msg.time}</span></p><p class="text">${msg.text}</p>`;
+    messages.appendChild(item);
+    window.scrollTo(0, document.body.scrollHeight);
+    console.log(msg);
 });
+
+function outputRoomName(room) {
+    roomName.innerText = room;
+}
+
+function outputUsers(users) {
+    userList.innerHTML = `${users.map(user => `<li>${user.username}</li>`).join('')}`;
+}
