30c63633f6b2462092689028e0e973096e2e5f7d
Riley <94302477+Rilaey@users.noreply.github.com>
2023-07-02 18:23:13 -0400

    fixed mid ui lol.

--- Develop/server.js
+++ Develop/server.js
@@ -3,20 +3,25 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const helpers = require('./utils/helpers')
+const helpers = require('./utils/helpers');
 //cardMiddleware = require('./utils/card');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const formatMessage = require('./utils/chatMessages');  //Socket.io requirement
-const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./utils/chatUsers');  //Socket.io requirement
+const formatMessage = require('./utils/chatMessages'); //Socket.io requirement
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+} = require('./utils/chatUsers'); //Socket.io requirement
 
-const httpServer = require("http").createServer(app);
-const io = require("socket.io")(httpServer, {
+const httpServer = require('http').createServer(app);
+const io = require('socket.io')(httpServer, {
   cors: {
-    credentials: true
+    credentials: true,
   },
 });
 
@@ -34,8 +39,8 @@ const sess = {
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
-  })
+    db: sequelize,
+  }),
 };
 
 app.use(session(sess));
@@ -49,31 +54,46 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
-io.on('connection', (socket) => { //Socket.io requirement
-  socket.on('joinRoom', ({ chatUsername, teamRoom}) => {
+io.on('connection', (socket) => {
+  //Socket.io requirement
+  socket.on('joinRoom', ({ chatUsername, teamRoom }) => {
     const user = userJoin(socket.id, chatUsername, teamRoom);
     socket.join(user.teamRoom);
-    socket.emit('chat message', formatMessage('CHATBOT\n', `WELCOME TO ${user.teamRoom}`));
-    socket.broadcast.to(user.teamRoom).emit('chat message', formatMessage('CHATBOT\n', `${user.chatUsername} HAS JOINED`));
+    socket.emit(
+      'chat message',
+      formatMessage('CHATBOT\n', `WELCOME TO ${user.teamRoom}`)
+    );
+    socket.broadcast
+      .to(user.teamRoom)
+      .emit(
+        'chat message',
+        formatMessage('CHATBOT\n', `${user.chatUsername} HAS JOINED`)
+      );
     io.to(user.teamRoom).emit('roomUsers', {
       teamRoom: user.teamRoom,
-      users: getRoomUsers(user.teamRoom)
+      users: getRoomUsers(user.teamRoom),
     });
   });
-  
-  socket.on('chat message', msg => {
+
+  socket.on('chat message', (msg) => {
     const user = getCurrentUser(socket.id);
-    io.to(user.teamRoom).emit('chat message', formatMessage(user.chatUsername + '\n', msg));
+    io.to(user.teamRoom).emit(
+      'chat message',
+      formatMessage(user.chatUsername + '\n', msg)
+    );
   });
 
   socket.on('disconnect', () => {
     const user = userLeave(socket.id);
 
     if (user) {
-      io.to(user.teamRoom).emit('chat message', formatMessage('CHATBOT\n', `${user.chatUsername} HAS LEFT`));
+      io.to(user.teamRoom).emit(
+        'chat message',
+        formatMessage('CHATBOT\n', `${user.chatUsername} HAS LEFT`)
+      );
       io.to(user.teamRoom).emit('roomUsers', {
         teamRoom: user.teamRoom,
-        users: getRoomUsers(user.teamRoom)
+        users: getRoomUsers(user.teamRoom),
       });
     }
   });
